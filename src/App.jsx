e7ad0c6fd413bad78ef59beada29afd74664b3ad
Dylan Cordero Mena <dsoultime@gmail.com>
2025-06-25 16:21:06 -0600

    Pruebita
    Primer push

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+import Guitar from "./Components/Guitar"
+import Header from "./Components/Header"
+import { db } from "./data/db"
+
+function App() {
+  const [data, setData] = useState([])
+
+    // useEffect(() => {  <---- para apis no locales
+    //   setData(db)
+    // }, [])
+  
+  return (
+    <>
+
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+        </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
+
+    </>
+  )
+}
+
+export default App
