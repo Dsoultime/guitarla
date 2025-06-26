7854f756a6d4f7d7298c8544dd6c0667c45bf81d
Dylan Cordero Mena <dsoultime@gmail.com>
2025-06-25 22:37:24 -0600

    ok
    Segundo push

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Guitar from "./Components/Guitar"
 import Header from "./Components/Header"
 import { db } from "./data/db"
 
 function App() {
-  const [data, setData] = useState([])
 
-    // useEffect(() => {  <---- para apis no locales
-    //   setData(db)
-    // }, [])
-  
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+  // const [data, setData] = useState([])
+  // useEffect(() => {  <---- para apis no locales
+  //   setData(db)
+  // }, [])
+  function addToCart(item) {
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExists >= 0) {
+      const updateCart = [...cart]
+      updateCart[itemExists].quantity++
+      setCart(updateCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+
+  }
+
+  function removeFromCart(id){
+   setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
   return (
     <>
-
-      <Header />
+      <Header 
+      cart={cart}
+      removeFromCart={removeFromCart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {data.map((guitar) => ( //con map siempre hay que traer la key 
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
-
 
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
